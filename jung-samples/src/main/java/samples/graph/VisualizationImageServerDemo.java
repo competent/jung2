903c2845a24af77240ed794aa8b1232b06683339
tomnelson <tomnelson>
2007-05-04 15:19:31 +0000

    remove commented stuff

--- jung-samples/src/main/java/samples/graph/VisualizationImageServerDemo.java
+++ jung-samples/src/main/java/samples/graph/VisualizationImageServerDemo.java
@@ -8,42 +8,23 @@
  */
 package samples.graph;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
 import java.awt.geom.Point2D;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 
-import org.apache.commons.collections15.Transformer;
 import org.apache.commons.collections15.functors.ConstantTransformer;
 
 import edu.uci.ics.jung.algorithms.layout.KKLayout;
 import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
 import edu.uci.ics.jung.graph.util.EdgeType;
-import edu.uci.ics.jung.visualization.GraphMouseListener;
-import edu.uci.ics.jung.visualization.Layer;
 import edu.uci.ics.jung.visualization.VisualizationImageServer;
-import edu.uci.ics.jung.visualization.VisualizationServer;
-import edu.uci.ics.jung.visualization.GraphZoomScrollPane;
-import edu.uci.ics.jung.visualization.VisualizationViewer;
-import edu.uci.ics.jung.visualization.control.AbstractModalGraphMouse;
-import edu.uci.ics.jung.visualization.control.CrossoverScalingControl;
-import edu.uci.ics.jung.visualization.control.DefaultModalGraphMouse;
-import edu.uci.ics.jung.visualization.control.ScalingControl;
 import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
 import edu.uci.ics.jung.visualization.renderers.GradientVertexRenderer;
 import edu.uci.ics.jung.visualization.renderers.Renderer;
@@ -51,13 +32,6 @@ import edu.uci.ics.jung.visualization.renderers.BasicVertexLabelRenderer.InsideP
 
 
 /**
- * Demonstrates the use of <code>GraphZoomScrollPane</code>.
- * This class shows the <code>VisualizationViewer</code> zooming
- * and panning capabilities, using horizontal and
- * vertical scrollbars.
- *
- * <p>This demo also shows ToolTips on graph vertices and edges,
- * and a key listener to change graph mouse modes.</p>
  * 
  * @author Tom Nelson
  * 
@@ -75,8 +49,6 @@ public class VisualizationImageServerDemo {
     VisualizationImageServer<String, Number> vv;
     
     /**
-     * create an instance of a simple graph with controls to
-     * demo the zoom features.
      * 
      */
     public VisualizationImageServerDemo() {
@@ -85,58 +57,9 @@ public class VisualizationImageServerDemo {
         graph = new DirectedSparseMultigraph<String, Number>();
         String[] v = createVertices(10);
         createEdges(v);
-        
-//        ImageIcon sandstoneIcon = null;
-//        String imageLocation = "/images/Sandstone.jpg";
-//        try {
-//            	sandstoneIcon = 
-//            	    new ImageIcon(getClass().getResource(imageLocation));
-//        } catch(Exception ex) {
-//            System.err.println("Can't load \""+imageLocation+"\"");
-//        }
-//        final ImageIcon icon = sandstoneIcon;
+
         vv =  new VisualizationImageServer<String,Number>(new KKLayout<String,Number>(graph), new Dimension(600,600));
-        
-//        if(icon != null) {
-//            vv.addPreRenderPaintable(new VisualizationServer.Paintable(){
-//                public void paint(Graphics g) {
-//                    Dimension d = vv.getSize();
-//                    g.drawImage(icon.getImage(),0,0,d.width,d.height,vv);
-//                }
-//                public boolean useTransform() { return false; }
-//            });
-//        }
-//        vv.addPostRenderPaintable(new VisualizationServer.Paintable(){
-//            int x;
-//            int y;
-//            Font font;
-//            FontMetrics metrics;
-//            int swidth;
-//            int sheight;
-//            String str = "GraphZoomScrollPane Demo";
-//            
-//            public void paint(Graphics g) {
-//                Dimension d = vv.getSize();
-//                if(font == null) {
-//                    font = new Font(g.getFont().getName(), Font.BOLD, 30);
-//                    metrics = g.getFontMetrics(font);
-//                    swidth = metrics.stringWidth(str);
-//                    sheight = metrics.getMaxAscent()+metrics.getMaxDescent();
-//                    x = (d.width-swidth)/2;
-//                    y = (int)(d.height-sheight*1.5);
-//                }
-//                g.setFont(font);
-//                Color oldColor = g.getColor();
-//                g.setColor(Color.lightGray);
-//                g.drawString(str, x, y);
-//                g.setColor(oldColor);
-//            }
-//            public boolean useTransform() {
-//                return false;
-//            }
-//        });
-//
-//        vv.addGraphMouseListener(new TestGraphMouseListener<String>());
+
         vv.getRenderer().setVertexRenderer(
         		new GradientVertexRenderer<String,Number>(
         				Color.white, Color.red, 
@@ -147,59 +70,15 @@ public class VisualizationImageServerDemo {
         vv.getRenderContext().setArrowFillPaintTransformer(new ConstantTransformer(Color.lightGray));
         vv.getRenderContext().setArrowDrawPaintTransformer(new ConstantTransformer(Color.lightGray));
         
-        // add my listeners for ToolTips
-//        vv.setVertexToolTipTransformer(new ToStringLabeller());
-//        vv.setEdgeToolTipTransformer(new Transformer<Number,String>() {
-//			public String transform(Number edge) {
-//				return "E"+graph.getEndpoints(edge).toString();
-//			}});
-        
         vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
         vv.getRenderer().getVertexLabelRenderer().setPositioner(new InsidePositioner());
         vv.getRenderer().getVertexLabelRenderer().setPosition(Renderer.VertexLabel.Position.AUTO);
-//        vv.setForeground(Color.lightGray);
-        
+
         // create a frome to hold the graph
         final JFrame frame = new JFrame();
         Container content = frame.getContentPane();
-//        final GraphZoomScrollPane panel = new GraphZoomScrollPane(vv);
-      //  content.add(vv);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        final AbstractModalGraphMouse graphMouse = new DefaultModalGraphMouse();
-     //   vv.setGraphMouse(graphMouse);
-        
-    //    vv.addKeyListener(graphMouse.getModeKeyListener());
-   //     vv.setToolTipText("<html><center>Type 'p' for Pick mode<p>Type 't' for Transform mode");
-        
-//        final ScalingControl scaler = new CrossoverScalingControl();
-
-//        JButton plus = new JButton("+");
-//        plus.addActionListener(new ActionListener() {
-//            public void actionPerformed(ActionEvent e) {
-//                scaler.scale(vv.getServer(), 1.1f, vv.getCenter());
-//            }
-//        });
-//        JButton minus = new JButton("-");
-//        minus.addActionListener(new ActionListener() {
-//            public void actionPerformed(ActionEvent e) {
-//                scaler.scale(vv.getServer(), 1/1.1f, vv.getCenter());
-//            }
-//        });
-
-//        JButton reset = new JButton("reset");
-//        reset.addActionListener(new ActionListener() {
-//
-//			public void actionPerformed(ActionEvent e) {
-//				vv.getRenderContext().getMultiLayerTransformer().getTransformer(Layer.LAYOUT).setToIdentity();
-//				vv.getRenderContext().getMultiLayerTransformer().getTransformer(Layer.VIEW).setToIdentity();
-//			}});
-
-//        JPanel controls = new JPanel();
-//        controls.add(plus);
-//        controls.add(minus);
-//        controls.add(reset);
-//        content.add(controls, BorderLayout.SOUTH);
-//
         
         Image im = vv.getImage(new Point2D.Double(300,300), new Dimension(600,600));
         Icon icon = new ImageIcon(im);
@@ -245,23 +124,6 @@ public class VisualizationImageServerDemo {
         graph.addEdge(new Double(Math.random()), v[6], v[5], EdgeType.DIRECTED);
         graph.addEdge(new Double(Math.random()), v[4], v[2], EdgeType.DIRECTED);
         graph.addEdge(new Double(Math.random()), v[5], v[4], EdgeType.DIRECTED);
-    }
-
-    /**
-     * A nested class to demo the GraphMouseListener finding the
-     * right vertices after zoom/pan
-     */
-    static class TestGraphMouseListener<V> implements GraphMouseListener<V> {
-        
-    		public void graphClicked(V v, MouseEvent me) {
-    		    System.err.println("Vertex "+v+" was clicked at ("+me.getX()+","+me.getY()+")");
-    		}
-    		public void graphPressed(V v, MouseEvent me) {
-    		    System.err.println("Vertex "+v+" was pressed at ("+me.getX()+","+me.getY()+")");
-    		}
-    		public void graphReleased(V v, MouseEvent me) {
-    		    System.err.println("Vertex "+v+" was released at ("+me.getX()+","+me.getY()+")");
-    		}
     }
 
     /**
